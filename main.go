7be43397e0b70088125449e34d50a8ae8ba34fed
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-02-02 16:35:56 +0300

    added main func calling

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"image"
+	"image/png"
+	"log"
+	"os"
+	"pngMerger/config"
+	"pngMerger/imager"
+)
 
+func main() {
+	loadConfig, err := config.LoadConfig()
+	if err != nil {
+		return
+	}
+
+	images := make([]image.Image, 0, 0)
+	for _, specs := range loadConfig.Images {
+		pngImage, err := os.Open(specs.Filename)
+		if err != nil {
+			panic(err.Error())
+		}
+		src, _, err := image.Decode(pngImage)
+		if err != nil {
+			panic(err.Error())
+		}
+		pngImage.Close()
+
+		if specs.ColorOld != "" && specs.ColorNew != "" {
+			newImg, err := imager.ReplaceHexColors(src, specs.ColorOld, specs.ColorNew)
+			if err != nil {
+				log.Println(err)
+			} else {
+				images = append(images, newImg)
+				continue
+			}
+		}
+
+		images = append(images, src)
+	}
+
+	mergedImg, err := imager.MergePng(images)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	outfile, err := os.Create("newPng.png")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer outfile.Close()
+
+	png.Encode(outfile, mergedImg)
 }
